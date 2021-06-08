de376b827150029a82d9ff6fb9fd8e392ae260eb
Eriberto Guzman <eriberto.guzman@outlook.com>
2021-06-07 21:20:08 -0400

    Adds drawio file that explains the node tree and sets up some of the dcomponents for the app.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import NavBar from './components/NavBar';
+import PokemonsContainer from './containers/PokemonsContainer';
+import TeamContainer from './containers/TeamContainer';
 
 function App() {
   return (
     <div className="App">
-
+        <NavBar />
+        {true ? <PokemonsContainer /> : <TeamContainer />}
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,8 @@ import React from 'react'
 function NavBar() {
     return (
         <div>
-            
+            <button><h1>Pokemons</h1></button>
+            <button><h1>View Team</h1></button>
         </div>
     )
 }
