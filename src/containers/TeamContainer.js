167aef190d263d096ecf92fb5d8a1e89455fd7c3
Eriberto Guzman <eriberto.guzman@outlook.com>
2021-06-08 07:55:18 -0400

    MMapps out the props received from the teams container.

--- src/containers/TeamContainer.js
+++ src/containers/TeamContainer.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
-function TeamContainer() {
+function TeamContainer(props) {
+    const mappedTeam = props.teams.map(pokemon => (
+        <div className='card'>
+            <p>Name: {pokemon.name}</p>
+            <p>Type: {pokemon.type}</p>
+            <img src={pokemon.back_image} />
+        </div>)
+    )
+    
     return (
         <div id="team-container">
-            
+            {mappedTeam}
         </div>
     )
 }
-
+    
 export default TeamContainer
